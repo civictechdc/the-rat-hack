b7caac1060ce011e4cba56f2b53d6c2f611bb59a
Zoheb Nensey <zoheb.nensey@gmail.com>
2017-02-23 21:08:32 -0500

    first version of merge code, needs refining

--- scripts/restaurant_data_cleaning.R
+++ scripts/restaurant_data_cleaning.R
@@ -10,3 +10,17 @@ opentable_api_mappings <- read.csv("restaurant_code_violations/opentable_crosswa
 yelp_api_mappings <- read.csv("restaurant_code_violations/yelp_crosswalk.csv")
 violations_labels <- read.csv("restaurant_code_violations/violations_key.csv")
 violations_data <- read.csv("restaurant_code_violations/violations.csv")
+
+# Bind together the violations data and labels. 
+violations_detailed <- merge(violations_data,violations_labels,
+                             by.x="Violation.Number",by.y="Violation.Number")
+
+# Bind together the inspections and the detailed violations data
+inspections_detailed <- merge(violations_detailed,inspections_data,
+                              by.x='Inspection.ID',by.y='Inspection.ID')
+
+# Pull in the crosswalks
+inspections_with_yelp <- merge(inspections_detailed,yelp_api_mappings,
+                               by.x='Permit.ID',by.y='PermitID')
+inspections_with_crosswalks <- merge(inspections_with_yelp,opentable_api_mappings,
+                                     by.x='Permit.ID',by.y='PermitID')
