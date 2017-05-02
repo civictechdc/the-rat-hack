eeab6aaf1835f9a03e797c84a963b4ad65b2b0bc
Jason Asher <jason.m.asher@gmail.com>
2017-05-01 20:57:41 -0400

    Initial Commit
    First 311 Portal shiny app version
    Includes pre-computed summarized 311 data for 2016

---
+++ scripts/demo_shiny_app/demo_shiny_app.R
@@ -0,0 +1,169 @@
+library(shiny)
+library(leaflet)
+library(rgdal)
+library(tidyverse)
+library(ggthemes)
+library(forcats)
+
+setwd(getSrcDirectory(function(x){}))
+
+# created from write_importData_for_shiny_app.R 
+# cols: service_code, service_code_description, time_aggregation_unit, time_aggregation_value, year, census_tract, count
+data_file = "./data/dc_311-2016_summarized.csv"
+summarized_data = read_csv(data_file, col_types = cols(service_code = col_character()))
+
+# cols: time_aggregation_unit, time_aggregation_value, year, census_tract, totalrequests # TODO: consider renaming last column to total_requests
+total_requests_data_file = "./data/dc_311-2016_totalrequests.csv"
+total_request_data = read_csv(total_requests_data_file) %>%
+  rename(total_requests = totalrequests)
+
+census_tract_data = readOGR("../../data/dc_census_tract_shapefiles/census_2010/", "gz_2010_11_140_00_500k")
+
+service_codes_and_descriptions = summarized_data %>%
+  distinct(service_code, service_code_description)
+
+ui <- navbarPage(title = "311 Portal",
+                 tabPanel("Explore",
+                          tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}
+                                     #controls {opacity: 0.85; padding: 10px}
+                                     #controls:hover {opacity: 1.0}"), # Setting map height this way (CSS3) to fill screen in the tab panel
+                          leafletOutput("map", width = "100%", height = "100%"),
+                          absolutePanel(id = "controls", class = "panel panel-default", top = 60, right = 20,
+                                        selectInput("selected_service_code", "Service Request Type",
+                                                    setNames(service_codes_and_descriptions$service_code,
+                                                             service_codes_and_descriptions$service_code_description)),
+                                        checkboxInput("normalize_by_total_requests", "Normalize by Total Requests", FALSE),
+                                        sliderInput("selected_time_aggregation_value", "Month",
+                                                    min(summarized_data$time_aggregation_value),
+                                                    max(summarized_data$time_aggregation_value),
+                                                    value = min(summarized_data$time_aggregation_value), step = 1),
+                                        plotOutput("request_count_time_series_plot", height = 200))
+                          ),
+                 tabPanel("Compare") #TODO: Merge Elizabeth's code
+                 )
+
+# ui <- bootstrapPage(
+#   tags$style(type = "text/css", "html, body {width:100%;height:100%} 
+#                                      #controls {opacity:0.85;padding:10px}
+#              #controls:hover {opacity:1.0}"),
+#   leafletOutput("map", width = "100%", height = "100%"),
+#   absolutePanel(id = "controls", class = "panel panel-default", top = 60, right = 20,
+#                 selectInput("selected_service_code", "Service Request Type",
+#                             setNames(service_codes_and_descriptions$service_code,
+#                                      service_codes_and_descriptions$service_code_description)),
+#                 sliderInput("selected_time_aggregation_value", "Month",
+#                             min(summarized_data$time_aggregation_value),
+#                             max(summarized_data$time_aggregation_value),
+#                             value = min(summarized_data$time_aggregation_value), step = 1),
+#                 plotOutput("request_count_time_series_plot", height = 200))
+# )
+
+server <- function(input, output, session) {
+  
+  # Data for selected service code
+  selected_service_code_data = reactive({
+    summarized_data %>%
+      filter(service_code == input$selected_service_code) %>%
+      left_join(total_request_data, by = c("year", "census_tract", "time_aggregation_value")) # adds 'totalrequests' column
+  })
+  
+  # Geographic data with the number of requests for the selected service code mapped to census tract
+  map_data <- reactive({
+    census_tract_data %>%
+      merge(selected_service_code_data() %>%
+              filter(time_aggregation_value == input$selected_time_aggregation_value,
+                     !is.na(census_tract)) %>%
+              rename(TRACT = census_tract) %>%
+              mutate(map_metric = if(input$normalize_by_total_requests){count/total_requests}else{count}),
+            by = "TRACT")
+  })
+  
+  # Data for the time series chart of requests for the selected service code
+  time_series_data <- reactive({
+    selected_service_code_data() %>%
+      group_by(time_aggregation_value) %>%
+      summarize(count = sum(count),
+                total_requests = sum(total_requests)) %>%
+      mutate(is_selected = ifelse(time_aggregation_value == input$selected_time_aggregation_value, TRUE, FALSE))
+    })
+  
+  # Initialize map
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      setView(lng = -77.0369, lat = 38.9072, zoom = 12) %>%
+      #addProviderTiles("Stamen.TonerLite")
+      addProviderTiles("CartoDB.PositronNoLabels")
+  })
+  
+  # Color palette that is updated to match the range of values for the selected service code
+  palette <- reactive({
+    if (!input$normalize_by_total_requests) {
+      colorBin(
+        palette = "plasma",
+        domain = selected_service_code_data() %>%
+          filter(!is.na(census_tract)) %>%
+          select(count) %>%
+          unlist %>%
+          range
+      ) 
+    } else {
+      colorBin(
+        palette = "plasma",
+        domain = selected_service_code_data() %>%
+          filter(!is.na(census_tract)) %>%
+          mutate(relative_count = count / total_requests) %>%
+          select(relative_count) %>%
+          unlist %>%
+          range
+      ) 
+    }
+  })
+  
+  # Update polygons when service code or month/week is changed
+  observe({
+    pal <- palette()
+    
+    leafletProxy("map", data = map_data()) %>%
+      addPolygons(
+        stroke = TRUE,
+        color = "black",
+        weight = 1,
+        fillColor = ~pal(map_metric),
+        fillOpacity = 0.7,
+        smoothFactor = 0.5,
+        layerId = ~TRACT, # Using a layerId enables efficient redrawing without clearing polygons
+        label = ~paste0("Tract ", substr(TRACT, 3, 6),
+                        ": ", count, ifelse(is.na(count) | count != 1, " requests", " request")),
+        highlightOptions = highlightOptions(color = "white", weight = 2, bringToFront = TRUE)
+      )
+  })
+  
+  # Update legend when service code is changed
+  observe({
+    leafletProxy("map") %>%
+      clearControls() %>%
+      addLegend("bottomleft", pal = palette(),
+                values = ifelse(input$normalize_by_total_requests,
+                                selected_service_code_data()$count/selected_service_code_data()$total_requests,
+                                selected_service_code_data()$count),
+                title = if(input$normalize_by_total_requests){"Request Fraction"}else{"Requests"}, opacity = 1)
+  })
+  
+  # Update time series chart
+  output$request_count_time_series_plot <- renderPlot({
+    p = ggplot(time_series_data()) +
+      geom_bar(aes(x = as.factor(time_aggregation_value),
+                   y = if(input$normalize_by_total_requests){count/total_requests}else{count}, fill = is_selected),
+               stat = "identity", show.legend = FALSE) +
+      scale_fill_manual(values = c("black", tableau_color_pal(palette = "tableau10")(1))) +
+      labs(title = if(input$normalize_by_total_requests){"Request Fraction over Time"}else{"Service Requests over Time"}, x = "Month", y = "") +
+      theme_bw(base_size = 14)
+    if(input$normalize_by_total_requests){
+      p + scale_y_continuous(labels = scales::percent)
+    } else {
+      p
+    }
+  })
+}
+
+shinyApp(ui, server)
