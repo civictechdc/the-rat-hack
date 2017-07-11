adaf193fe19aaa4fbd2e97c3e5747c28575ca5b7
Elizabeth Lee <eclee25@gmail.com>
2017-07-10 22:52:28 -0400

    change geom_point back to original geom_bar
    app.R
    - change time series back to bar graph

--- scripts/demo_shiny_app/app.R
+++ scripts/demo_shiny_app/app.R
@@ -196,7 +196,7 @@ server <- function(input, output, session) {
   update_request_time_series_plot <- function(selected_time_series_data,
                                               normalize_by_total_requests){
     p = ggplot(selected_time_series_data) +
-      geom_point(aes(x = as.factor(time_aggregation_value),
+      geom_bar(aes(x = as.factor(time_aggregation_value),
                          y = if(normalize_by_total_requests){count/total_requests}else{count}, colour = is_selected), show.legend = FALSE) +
       scale_colour_manual(values = c("black", tableau_color_pal(palette = "tableau10")(1))) +
       labs(title = if(normalize_by_total_requests){"Request Percentage over Time"}else{"Service Requests over Time"}, x = "Month", y = "") +
